d0623f1b0d22ff4a03b6ad45d7bc8a5020874d18
ken.huang <979782267@qq.com>
2017-11-22 17:28:21 +0800

    添加新项目

---
+++ reader/src/main/java/com/kenzz/reader/utils/StatusBarView.java
@@ -0,0 +1,20 @@
+package com.kenzz.reader.utils;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by ken.huang on 11/22/2017.
+ */
+
+public class StatusBarView extends View {
+    public StatusBarView(Context context) {
+        super(context);
+    }
+
+    public StatusBarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
